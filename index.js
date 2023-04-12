20ec3cfb2f1c68bfb78e9f123eb45a34d3a2c7f7
dweltstorm <90920200+dweltstorm@users.noreply.github.com>
2023-04-12 13:00:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var url = new URL(window.location.href).searchParams.get('url') || 'test';
+var url = new URL(window.location.href).searchParams.get('url'); url ??= 'https://google.com';
 //var frame = document.createElement('iframe'); frame.src = url || 'https://google.com'; frame.style = "position:absolute;top:0;left:0;width:100vw;height:100vh;border:0;margin:0;"
 //document.body.appendChild(frame);
 alert(url);
