00c3b20cc496a5c8c3968f94947deed85b91ab9a
dweltstorm <90920200+dweltstorm@users.noreply.github.com>
2023-04-12 15:17:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-
-var url = new URL(window.location.href).searchParams.get('url');
-var frame = document.getElementById('frame');
-
-if(url == null) {
-  frame.src = prompt('URL:');
-} else {
-  frame.src = url;
-}
+document.getElementById('frame').src = prompt("Enter URL here (https:// required)");
