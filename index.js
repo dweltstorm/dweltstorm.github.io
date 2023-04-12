951702c7023ca185b06ea3076aef5cbad2ba8f25
dweltstorm <90920200+dweltstorm@users.noreply.github.com>
2023-04-12 09:35:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var url = prompt('URL:')
-var frame = document.createElement('iframe'); frame.src = url; frame.style = "display:block;width:100vw;height:100vh;border:none;"
+var frame = document.createElement('iframe'); frame.src = url; frame.style = "position:absolute;top:0;left:0;width:100%;height:100%;border:0"
 document.body.appendChild(frame);
