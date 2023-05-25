dea1b2b4b8520ea37bf458f447164e36dccad1f5
dweltstorm <90920200+dweltstorm@users.noreply.github.com>
2023-05-25 02:42:00 -0700

    added geogun

--- index.js
+++
@@ -1,2 +0,0 @@
-var frame = document.createElement('iframe'); frame.src = prompt("Enter URL here (https:// required)"); frame.style = "position:absolute;top:0;left:0;width:100vw;height:100vh;border:0;margin:0;";
-document.body.appendChild(frame);
